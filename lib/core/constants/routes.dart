be79b62d80a8b525c2538698d8b378381cea4b37
PHLY101 <basel6889@gmail.com>
2026-02-17 16:12:52 +0200

    + routes.dart

---
+++ lib/core/constants/routes.dart
@@ -0,0 +1,51 @@
+class AppRoutes {
+  //? splash screen
+  static const String splash = '/';
+
+  //? onBoarding
+  static const String onBoarding1 = '/onBoarding1';
+  static const String onBoarding2 = '/onBoarding2';
+  static const String onBoarding3 = '/onBoarding3';
+
+  //? Authentication Screens
+  static const String signUp = '/signUp';
+  static const String login = '/login';
+  static const String verification = '/verification';
+  static const String forgotPassword = '/forgotPassword';
+
+  //? Home Screen
+  static const String home = '/home';
+
+  //? Assessment screen
+  static const String assessment = '/assessment';
+
+  //? Course screens
+  static const String course = '/course';
+  static const String playList = '/playList';
+  static const String lessonQuiz = '/lessonQuiz';
+  static const String quizResults = '/quizResults';
+  static const String lessonQuizAnswers =
+      '/lessonQuizAnswers'; // not sure about it yet...
+
+  //? agenda screens
+  static const String agenda = '/agenda';
+
+  //? notifications screens
+  static const String notifications = '/notifications';
+
+  //? library screens
+  static const String library = '/library';
+
+  //? profile screens
+  static const String profile = '/profile';
+  static const String badges = '/badges';
+  static const String achievement = '/achievement';
+  static const String contactUs = '/contactUs';
+  static const String settings = '/settings';
+
+  //? Contest screens
+  static const String contestScreen = '/contestScreen';
+  static const String contestResults = '/contestResults';
+  static const String contestReview = '/contestReview';
+  static const String contestLeaderboards = '/contestLeaderBoards';
+}
