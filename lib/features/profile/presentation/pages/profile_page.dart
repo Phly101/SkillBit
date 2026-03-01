d6cadb2cbb0a484d29a882943de18b0ae1031a31
PHLY101 <basel6889@gmail.com>
2026-03-01 21:52:14 +0200

    init profile page

--- lib/features/profile/presentation/pages/profile_page.dart
+++ lib/features/profile/presentation/pages/profile_page.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:skill_bit/core/theme/theme.dart';
+import 'package:skill_bit/features/profile/presentation/widgets/components/profile_header.dart';
+
+import '../../../../core/utils/assets.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
   @override
   Widget build(final BuildContext context) {
-    return const Placeholder(
-      color: Colors.blue,
-    );
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          ProfileHeader(
+            profileUrl: Assets.image('Basel_EL_Rafei.jpeg'),
+            userName: 'Basel',
+            userPoints: '200 points',
+          ),
+        ],
+      ),
+    ).pH(20);
   }
 }
