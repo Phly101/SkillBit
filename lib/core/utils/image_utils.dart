a6042b85d4047744fb140e37cb02b1142b9b6a44
PHLY101 <basel6889@gmail.com>
2026-03-01 21:53:28 +0200

    created widgets for profile feature UI

---
+++ lib/core/utils/image_utils.dart
@@ -0,0 +1,58 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class ImageUtils {
+  ImageUtils._();
+
+  static void showHeroPreview(
+    final BuildContext context, {
+    required final String profileUrl,
+    required final String heroTag,
+  }) {
+    showGeneralDialog(
+      context: context,
+      barrierDismissible: true,
+      barrierLabel: 'Dismiss',
+      barrierColor: Colors.black.withValues(alpha: 0.75),
+      transitionDuration: const Duration(milliseconds: 400),
+      pageBuilder:
+          (
+            final BuildContext context,
+            final Animation<double> anim1,
+            final Animation<double> anim2,
+          ) {
+            return GestureDetector(
+              onTap: () => Navigator.pop(context),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Center(
+                  child: ScaleTransition(
+                    scale: CurvedAnimation(
+                      parent: anim1,
+                      curve: Curves.fastOutSlowIn,
+                    ),
+                    child: Hero(
+                      tag: heroTag,
+                      child: InteractiveViewer(
+                        child: Container(
+                          width: MediaQuery.of(context).size.width * 0.85,
+                          height: MediaQuery.of(context).size.width * 0.85,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30),
+                            image: DecorationImage(
+                              image: AssetImage(profileUrl),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+    );
+  }
+}
