99e795c9501b14b0f002355645c365cabe8d43dd
PHLY101 <basel6889@gmail.com>
2026-03-01 11:12:53 +0200

    edited level_button_widget.dart to accept more parameters to be more flexible with multiple layout and sizes

--- lib/features/home/presentation/widgets/common/level_button_widget.dart
+++ lib/features/home/presentation/widgets/common/level_button_widget.dart
@@ -4,7 +4,11 @@ import 'package:skill_bit/core/widgets/button_widget.dart';
 import '../../../../../core/theme/theme.dart';
 
 class LevelButtonWidget extends StatelessWidget {
-  const LevelButtonWidget({super.key, required this.level, required this.function});
+  const LevelButtonWidget({
+    super.key,
+    required this.level,
+    required this.function,
+  });
 
   final String level;
   final void Function()? function;
@@ -13,6 +17,10 @@ class LevelButtonWidget extends StatelessWidget {
   Widget build(final BuildContext context) {
     return ButtonWidget(
       function: function,
+      radius: 20,
+      paddingL: 20,
+      paddingR: 20,
+      hasBorder: true,
       child: Text(
         level,
         style: context.textTheme.displayMedium!.copyWith(
