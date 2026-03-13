73e4ab844f63a1ca6e025905507b9cafd06f0bae
PHLY101 <basel6889@gmail.com>
2026-03-13 19:00:33 +0200

    added parameter top three list of leaderboard entity

--- lib/features/profile/presentation/pages/profile_page/widgets/components/profile_body.dart
+++ lib/features/profile/presentation/pages/profile_page/widgets/components/profile_body.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:skill_bit/core/theme/theme.dart';
-import '../common/best_ranking_widget.dart';
+import '../../../../../../../core/entities/leaderboard_entity.dart';
+import '../../../../../../../core/widgets/best_ranking_widget.dart';
 import '../common/growth_graph_widget.dart';
 import '../common/progress_pie_char_widget.dart';
 
 class ProfileBody extends StatelessWidget {
-  const ProfileBody({super.key, this.hasBestRank = false});
+  const ProfileBody({
+    super.key,
+    this.hasBestRank = false,
+    required this.topThree,
+  });
 
   final bool hasBestRank;
+  final List<LeaderboardEntity> topThree;
 
   @override
   Widget build(final BuildContext context) {
     return Column(
       mainAxisAlignment: .center,
       children: <Widget>[
-        if (hasBestRank) ...<Widget>[const BestRankingWidget()],
+        if (hasBestRank) ...<Widget>[
+          BestRankingWidget(topThree: topThree, isProfile: true),
+        ],
         80.heightBox,
         const GrowthGraphWidget(),
         20.heightBox,
