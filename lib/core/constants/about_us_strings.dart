296d23e74f9fa001cd9937e2ee41ba7605974255
PHLY101 <basel6889@gmail.com>
2026-03-13 18:43:30 +0200

    added about and bio preview strings

--- lib/core/constants/about_us_strings.dart
+++ lib/core/constants/about_us_strings.dart
@@ -10,10 +10,10 @@ class AboutUsStrings {
   };
 
   static const Map<String, String> bioPreview = <String, String>{
-    'Menna': '1111',
-    'Basel': 'Basel is the team...',
-    'Hagar': '2222222',
-    'Ahmed': '3333333',
+    'Menna': 'I’m Menna Ahmed,...',
+    'Basel': 'I\'m Basel El Rafei,...',
+    'Hagar': 'I\'m Hagar Elewa,...',
+    'Ahmed': 'I\'m Ahmed El Saied,...',
   };
   static const Map<String, String> teamNames = <String, String>{
     'Ahmed': 'Ahmed El Saied',
@@ -23,13 +23,16 @@ class AboutUsStrings {
   };
   static const Map<String, String> aboutMembers = <String, String>{
     'Ahmed': 'Ahmed El Saied',
-    'Basel': 'Basel El Rafei',
+    'Basel':
+        'I\'m BaseL EL Rafei, and I was responsible for the Idea,it\'s flutter implementation and the UX research and I also helped and guided the UI design ensuring the best user experience with best visuals for our valuable users',
     'Hagar': 'Hagar Elewa',
-    'Menna': 'Menna Ahmed',
+    'Menna':
+        'I’m Menna Ahmed, and I was responsible for the UI design and implementation of our graduation project application, ensuring a clean, modern, and user-friendly interface.',
   };
   static const Map<String, String> adviceMember = <String, String>{
     'Ahmed': 'Ahmed El Saied',
-    'Basel': 'Basel El Rafei',
+    'Basel':
+        'My advice is never lose hope in what you can achieve be curious and be patient',
     'Hagar': 'Hagar Elewa',
     'Menna': 'Menna Ahmed',
   };
