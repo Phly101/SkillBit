9db00d6a972535bfecbb68513c68f3f9ca87bf9a
PHLY101 <basel6889@gmail.com>
2026-03-13 19:21:44 +0200

    created friends entity

---
+++ lib/features/profile/domain/entities/friends_entity.dart
@@ -0,0 +1,14 @@
+class FriendsEntity {
+  FriendsEntity({
+     this.name,
+     this.profileUrl,
+     this.scorePoints,
+     this.progress,
+  });
+
+  final String? name;
+  final String? profileUrl;
+  final int? scorePoints;
+  final double? progress;
+}
+
