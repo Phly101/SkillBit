95022ce873ac491186444f378e82d045d7b16095
PHLY101 <basel6889@gmail.com>
2026-03-13 18:49:23 +0200

    increased the attributes of the class due to varying use cases of this class

--- lib/core/widgets/gradiant_container.dart
+++ lib/core/widgets/gradiant_container.dart
@@ -5,7 +5,7 @@ import 'package:skill_bit/core/theme/theme.dart';
 class GradiantContainer extends StatelessWidget {
   const GradiantContainer({
     super.key,
-    required this.child,
+    this.child,
     this.radius,
     this.width,
     this.height,
@@ -13,16 +13,30 @@ class GradiantContainer extends StatelessWidget {
     this.begin = .topCenter,
     this.end = .centerEnd,
     this.inverted = false,
+    this.doAllTakeBorder = true,
+    this.borderRadius,
+    this.topRight = 0,
+    this.topLeft = 0,
+    this.bottomRight = 0,
+    this.bottomLeft = 0,
+    this.border,
   });
 
-  final Widget child;
+  final Widget? child;
   final double? radius;
   final double? width;
   final double? height;
   final String? image;
   final bool inverted;
+  final double topRight;
+  final double topLeft;
+  final double bottomRight;
+  final double bottomLeft;
   final AlignmentGeometry begin;
   final AlignmentGeometry end;
+  final BorderRadiusGeometry? borderRadius;
+  final BoxBorder? border;
+  final bool doAllTakeBorder;
 
   @override
   Widget build(final BuildContext context) {
@@ -30,33 +44,37 @@ class GradiantContainer extends StatelessWidget {
       width: width,
       height: height,
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(radius ?? 0),
+        border: border,
+        borderRadius: doAllTakeBorder
+            ? .circular(radius ?? 0)
+            : .only(
+          topRight: .circular(topRight),
+          topLeft: .circular(topLeft),
+          bottomLeft: .circular(bottomLeft),
+          bottomRight: .circular(bottomRight),
+        ),
         image: image != null
-            ? DecorationImage(
-                image: Svg(
-                  image!,
-                ),
-                opacity: 0.4,
-                fit: .fill,
-              )
+            ? DecorationImage(image: Svg(image!), opacity: 0.4, fit: .fill)
             : null,
         gradient: inverted
             ? LinearGradient(
-                colors: <Color>[
-                  context.colorScheme.secondary,
-                  context.colorScheme.primary.withValues(alpha: 0.8),
-                  context.colorScheme.primary,
-                ],
-                begin: begin,
-                end: end,
-              )
+          colors: <Color>[
+            context.colorScheme.secondary,
+            context.colorScheme.primary.withValues(alpha: 0.8),
+            context.colorScheme.primary,
+          ],
+          begin: begin,
+          end: end,
+        )
             : LinearGradient(
-                colors: <Color>[
-                  context.colorScheme.primary,
-                  context.colorScheme.primary.withValues(alpha: 0.8),
-                  context.colorScheme.secondary,
-                ],
-              ),
+          colors: <Color>[
+            context.colorScheme.primary,
+            context.colorScheme.primary.withValues(alpha: 0.8),
+            context.colorScheme.secondary,
+          ],
+          begin: begin,
+          end: end,
+        ),
       ),
       child: child,
     );
