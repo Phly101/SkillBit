afdd977f41dee4f7b2acdf4019d74e4ea74e63db
PHLY101 <basel6889@gmail.com>
2026-03-13 18:56:25 +0200

    minor change

--- lib/features/profile/presentation/pages/about_us_page/widgets/member_card.dart
+++ lib/features/profile/presentation/pages/about_us_page/widgets/member_card.dart
@@ -3,8 +3,6 @@ import 'package:skill_bit/core/theme/theme.dart';
 import 'package:skill_bit/core/utils/image_utils.dart';
 import 'package:skill_bit/core/widgets/avatar_widget.dart';
 import 'package:skill_bit/core/widgets/shadow_container.dart';
-
-
 import '../../team_details_page/page/team_details_page.dart';
 
 
