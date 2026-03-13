d347d5689d503636bf1121a3996be635a0bdd4b9
PHLY101 <basel6889@gmail.com>
2026-03-13 19:10:29 +0200

    created contest entity

---
+++ lib/features/contests/domain/entities/contest_entity.dart
@@ -0,0 +1,33 @@
+import 'package:skill_bit/core/entities/question_entity.dart';
+import 'package:skill_bit/features/contests/domain/enums/contest_difficulty.dart';
+import 'package:skill_bit/features/contests/domain/enums/contest_status.dart';
+import 'package:skill_bit/features/contests/domain/enums/contest_type.dart';
+
+class ContestEntity {
+  ContestEntity({
+    required this.title,
+    required this.topic,
+    required this.contestType,
+    required this.contestDate,
+    required this.contestDuration,
+    required this.status,
+    required this.numberOfQuestions,
+    required this.totalContestScore,
+    required this.questions,
+    required this.contestDifficulty,
+  });
+
+  final String title;
+  final String topic;
+  final ContestType contestType;
+  final DateTime contestDate;
+  final Duration contestDuration;
+  final ContestDifficulty contestDifficulty;
+  final List<QuestionEntity> questions;
+  final int numberOfQuestions;
+  final double totalContestScore;
+  final ContestStatus status;
+
+  //Todo: challenger id and challenged id
+  //Todo: list of participants parameter
+}
