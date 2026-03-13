b8ae6c6fbfb8afb044f456842f5ecf3d8f20ce99
PHLY101 <basel6889@gmail.com>
2026-03-13 18:46:26 +0200

    added a size parameter that is used for variable icon size and added a safeguard for empty profileurls

--- lib/core/widgets/avatar_widget.dart
+++ lib/core/widgets/avatar_widget.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/material.dart';
-
 import '../theme/theme.dart';
+import '../utils/assets.dart';
 
 class AvatarWidget extends StatelessWidget {
-  const AvatarWidget({super.key, required this.profileUrl, this.radius = 25});
+  const AvatarWidget({
+    super.key,
+    required this.profileUrl,
+    this.radius = 25,
+    this.size = 35,
+  });
 
-  final String? profileUrl;
+  final String profileUrl;
   final double? radius;
+  final double size;
 
   @override
   Widget build(final BuildContext context) {
+    final bool hasImage = profileUrl.isNotEmpty;
+
     return CircleAvatar(
-      backgroundColor: profileUrl == null
-          ? context.colorScheme.outline
+      backgroundColor: !hasImage
+          ? context.colorScheme.outline.withValues(alpha: 0.5)
           : Colors.transparent,
       radius: radius,
-      backgroundImage: profileUrl != null ? AssetImage(profileUrl!) : null,
-      child: profileUrl == null ? const Icon(Icons.person, size: 35) : null,
+
+      backgroundImage: hasImage ? AssetImage(Assets.image(profileUrl)) : null,
+      child: !hasImage
+          ? Icon(Icons.person, size: size, color: context.colorScheme.onSurface)
+          : null,
     );
   }
 }
