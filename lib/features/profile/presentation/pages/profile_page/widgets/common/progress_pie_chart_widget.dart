17f50b7b9a233a4145a6acf53d7006e4b32ce4b1
PHLY101 <basel6889@gmail.com>
2026-03-13 20:15:21 +0200

    created new pie chart widget

---
+++ lib/features/profile/presentation/pages/profile_page/widgets/common/progress_pie_chart_widget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:skill_bit/core/widgets/pie_chart_widget.dart';
+
+class ProgressPieChartWidget extends StatelessWidget {
+  const ProgressPieChartWidget({
+    super.key,
+    required this.progressMade,
+    required this.progressLeft,
+    required this.title,
+  });
+
+  final double progressMade;
+  final double progressLeft;
+  final String title;
+
+  @override
+  Widget build(final BuildContext context) {
+    return PieChartWidget(
+      progressMade: progressMade,
+      progressLeft: progressLeft,
+      title: title,
+
+    );
+  }
+}
