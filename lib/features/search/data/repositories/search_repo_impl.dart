dc7b10204cfab6e4dc5db66406ebe8393ed0360c
PHLY101 <basel6889@gmail.com>
2026-03-14 13:53:43 +0200

    created search repo implementation

---
+++ lib/features/search/data/repositories/search_repo_impl.dart
@@ -0,0 +1,38 @@
+import 'package:dartz/dartz.dart';
+import 'package:skill_bit/core/error/failure.dart';
+import 'package:skill_bit/features/search/data/dataSources/search_local_data_source.dart';
+import 'package:skill_bit/features/search/domain/entities/search_courses_entity.dart';
+import 'package:skill_bit/features/search/domain/entities/search_friends_entity.dart';
+import 'package:skill_bit/features/search/domain/repositories/search_repo.dart';
+
+class SearchRepoImpl implements SearchRepository {
+  const SearchRepoImpl({required this.searchLocalDataSource});
+
+  final SearchLocalDataSource searchLocalDataSource;
+
+  @override
+  Either<Failure, List<SearchCourseEntity>> searchCourses(
+    final String courseQuery,
+  ) {
+    try {
+      final List<SearchCourseEntity> result = searchLocalDataSource
+          .searchCourses(courseQuery);
+      return Right<Failure, List<SearchCourseEntity>>(result);
+    } catch (e) {
+      return Left<Failure, List<SearchCourseEntity>>(CacheFailures());
+    }
+  }
+
+  @override
+  Either<Failure, List<SearchFriendsEntity>> searchFriends(
+    final String friendsQuery,
+  ) {
+    try {
+      final List<SearchFriendsEntity> result = searchLocalDataSource
+          .searchFriends(friendsQuery);
+      return Right<Failure, List<SearchFriendsEntity>>(result);
+    } catch (e) {
+      return Left<Failure, List<SearchFriendsEntity>>(CacheFailures());
+    }
+  }
+}
