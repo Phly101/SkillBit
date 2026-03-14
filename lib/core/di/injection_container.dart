abcc40b07237363b1ac950280c7e52b12a759db3
PHLY101 <basel6889@gmail.com>
2026-03-14 13:57:54 +0200

    created refactored the injection container to accept injection functions

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,46 +1,25 @@
+import 'package:skill_bit/core/di/injectors/search_injector.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import '../app_state/app_state_notifier.dart';
+import '../router/app_router.dart';
+import 'injectors/onboarding_injector.dart';
 
-import 'package:skill_bit/core/router/app_router.dart';
-import 'package:skill_bit/core/app_state/app_state_notifier.dart';
-
-import 'package:skill_bit/features/onboarding/data/dataSources/onboarding_local_data_source.dart';
-import 'package:skill_bit/features/onboarding/data/repositories/onboarding_repo_impl.dart';
-import 'package:skill_bit/features/onboarding/domain/repositories/onboarding_repo.dart';
-import 'package:skill_bit/features/onboarding/domain/useCases/has_on_boarded_use_case.dart';
-import 'package:skill_bit/features/onboarding/domain/useCases/complete_onboarding_use_case.dart';
-import 'package:skill_bit/features/onboarding/presentation/Bloc/onboarding_bloc.dart';
 
 final GetIt sl = GetIt.instance;
 
 Future<void> init() async {
-  //! External
+  //! External (Global dependencies)
   final SharedPreferences sharedPreferences =
       await SharedPreferences.getInstance();
-
   sl.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
 
-  //! Data Sources
-  sl.registerLazySingleton<OnboardingLocalDataSource>(
-    () => OnboardingLocalDataSourceImpl(sharedPreferences: sl()),
-  );
+  //! Features
+  initSearchFeature();
+  initOnboardingFeature();
 
-  //! Repository
-  sl.registerLazySingleton<OnboardingRepo>(
-    () => OnboardingRepoImpl(onboardingLocalDataSource: sl()),
-  );
 
-  //! Use Cases
-  sl.registerLazySingleton<HasOnBoarded>(() => HasOnBoarded(sl()));
-
-  sl.registerLazySingleton<CompleteOnboardingUseCase>(
-    () => CompleteOnboardingUseCase(sl()),
-  );
-
-  //! Feature Bloc
-  sl.registerFactory<OnboardingBloc>(() => OnboardingBloc(complete: sl()));
-
-  //! App State
+  //! App State (Core logic that spans features)
   sl.registerLazySingleton<AppStateNotifier>(
     () => AppStateNotifier(hasOnBoardedUseCase: sl()),
   );
