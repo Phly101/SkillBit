43e884f8de98f9cd0de962ca34d4571430a5092d
PHLY101 <basel6889@gmail.com>
2026-03-14 15:51:28 +0200

    deleted debouncer and implemented native bloc debouncer

--- lib/features/search/presentation/bloc/search_course_bloc/search_course_bloc.dart
+++ lib/features/search/presentation/bloc/search_course_bloc/search_course_bloc.dart
@@ -3,24 +3,33 @@ import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:skill_bit/core/error/failure.dart';
-import 'package:skill_bit/core/utils/search_debouncer.dart';
 import 'package:skill_bit/features/search/domain/entities/search_courses_entity.dart';
 import 'package:skill_bit/features/search/domain/useCases/params/search_course_params.dart';
 import 'package:skill_bit/features/search/domain/useCases/search_course_usecase.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part 'search_course_event.dart';
 
 part 'search_course_state.dart';
 
+const Duration _debounceDuration = Duration(milliseconds: 300);
+
+EventTransformer<E> debounce<E>(final Duration duration) {
+  return (final Stream<E> events, final EventMapper<E> mapper) =>
+      events.debounce(duration).switchMap(mapper);
+}
+
 class SearchCourseBloc extends Bloc<SearchCourseEvent, SearchCourseState> {
   SearchCourseBloc({required this.searchCourseUseCase})
     : super(SearchCourseInitial()) {
-    on<SearchCourseUpdated>(_onSearchUpdated);
+    on<SearchCourseUpdated>(
+      _onSearchUpdated,
+      transformer: debounce(_debounceDuration),
+    );
     on<SearchCourseCleared>(_onSearchCourseCleared);
   }
 
   final SearchCourseUseCase searchCourseUseCase;
-  final SearchDebouncer _debouncer = SearchDebouncer();
 
   Future<void> _onSearchUpdated(
     final SearchCourseUpdated event,
@@ -28,24 +37,22 @@ class SearchCourseBloc extends Bloc<SearchCourseEvent, SearchCourseState> {
   ) async {
     // init state
     if (event.query.isEmpty) {
-      _debouncer.cancel();
       emit(SearchCourseInitial());
       return;
     }
     // searching state
-    SearchDebouncer().run(() async {
-      emit(SearchCourseLoading());
-      final Either<Failure, List<SearchCourseEntity>> result =
-          searchCourseUseCase(SearchCourseParams(event.query));
-      result.fold(
-        //Failure case
-        (final Failure failure) =>
-            emit(const SearchCourseError(message: 'Failed to fetch Courses')),
-        //Success case
-        (final List<SearchCourseEntity> courses) =>
-            emit(SearchCourseSuccess(courses: courses)),
-      );
-    });
+
+    emit(SearchCourseLoading());
+    final Either<Failure, List<SearchCourseEntity>> result =
+        searchCourseUseCase(SearchCourseParams(event.query));
+    result.fold(
+      //Failure case
+      (final Failure failure) =>
+          emit(const SearchCourseError(message: 'Failed to fetch Courses')),
+      //Success case
+      (final List<SearchCourseEntity> courses) =>
+          emit(SearchCourseSuccess(courses: courses)),
+    );
   }
 
   // clearing the search filed function
@@ -53,7 +60,6 @@ class SearchCourseBloc extends Bloc<SearchCourseEvent, SearchCourseState> {
     final SearchCourseCleared event,
     final Emitter<SearchCourseState> emit,
   ) async {
-    _debouncer.cancel();
     final Either<Failure, List<SearchCourseEntity>> result =
         searchCourseUseCase(const SearchCourseParams(''));
 
--- lib/features/search/presentation/bloc/search_friends_bloc/search_friends_bloc.dart
+++ lib/features/search/presentation/bloc/search_friends_bloc/search_friends_bloc.dart
@@ -6,21 +6,30 @@ import 'package:skill_bit/core/error/failure.dart';
 import 'package:skill_bit/features/search/domain/entities/search_friends_entity.dart';
 import 'package:skill_bit/features/search/domain/useCases/params/search_friends_params.dart';
 import 'package:skill_bit/features/search/domain/useCases/search_friends_usecase.dart';
-import '../../../../../core/utils/search_debouncer.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part 'search_friends_event.dart';
 
 part 'search_friends_state.dart';
 
+const Duration _debounceDuration = Duration(milliseconds: 300);
+
+EventTransformer<E> debounce<E>(final Duration duration) {
+  return (final Stream<E> events, final EventMapper<E> mapper) =>
+      events.debounce(duration).switchMap(mapper);
+}
+
 class SearchFriendsBloc extends Bloc<SearchFriendsEvent, SearchFriendsState> {
   SearchFriendsBloc({required this.friendsUseCase})
     : super(SearchFriendsInitial()) {
-    on<SearchFriendsUpdated>(_onSearchFriendsUpdated);
+    on<SearchFriendsUpdated>(
+      _onSearchFriendsUpdated,
+      transformer: debounce(_debounceDuration),
+    );
     on<SearchFriendsCleared>(_onSearchFriendsCleared);
   }
 
   final SearchFriendsUseCase friendsUseCase;
-  final SearchDebouncer _debouncer = SearchDebouncer();
 
   // searching for a friend function
   Future<void> _onSearchFriendsUpdated(
@@ -29,27 +38,24 @@ class SearchFriendsBloc extends Bloc<SearchFriendsEvent, SearchFriendsState> {
   ) async {
     // init state
     if (event.query.isEmpty) {
-      _debouncer.cancel();
       emit(SearchFriendsInitial());
       return;
     }
     // searching state
-    SearchDebouncer().run(() async {
-      emit(SearchFriendsLoading());
-      final Either<Failure, List<SearchFriendsEntity>> result = friendsUseCase(
-        SearchFriendsParams(event.query),
-      );
 
-      result.fold(
-        //Failure case
-        (final Failure failure) => emit(
-          const SearchFriendsError(message: 'Could not find this friend'),
-        ),
-        //Success case
-        (final List<SearchFriendsEntity> friends) =>
-            emit(SearchFriendsSuccess(friends: friends)),
-      );
-    });
+    emit(SearchFriendsLoading());
+    final Either<Failure, List<SearchFriendsEntity>> result = friendsUseCase(
+      SearchFriendsParams(event.query),
+    );
+
+    result.fold(
+      //Failure case
+      (final Failure failure) =>
+          emit(const SearchFriendsError(message: 'Could not find this friend')),
+      //Success case
+      (final List<SearchFriendsEntity> friends) =>
+          emit(SearchFriendsSuccess(friends: friends)),
+    );
   }
 
   // clearing the search filed function
@@ -57,7 +63,6 @@ class SearchFriendsBloc extends Bloc<SearchFriendsEvent, SearchFriendsState> {
     final SearchFriendsCleared event,
     final Emitter<SearchFriendsState> emit,
   ) async {
-    _debouncer.cancel();
     final Either<Failure, List<SearchFriendsEntity>> result = friendsUseCase(
       const SearchFriendsParams(''),
     );
