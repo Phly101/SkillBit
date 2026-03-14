b9319b5b8d4bade63dbaad3c1c41faf23ac7652f
PHLY101 <basel6889@gmail.com>
2026-03-14 15:52:48 +0200

    added id

--- lib/features/course/domain/entities/course_entity.dart
+++ lib/features/course/domain/entities/course_entity.dart
@@ -1,11 +1,13 @@
 class CourseEntity {
 
   CourseEntity({
+    required this.id,
     required this.title,
     required this.imageUrl,
     required this.progress,
     required this.isLocked,
   });
+  final String id;
   final String title;
   final String imageUrl;
   final double progress;
