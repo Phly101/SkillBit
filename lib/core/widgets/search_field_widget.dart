77fa19ea1d69b0f4ce70354979a3132fdc5941d2
PHLY101 <basel6889@gmail.com>
2026-03-14 15:53:57 +0200

    edited the widget to accept more parameters to activate the search

--- lib/core/widgets/search_field_widget.dart
+++ lib/core/widgets/search_field_widget.dart
@@ -4,14 +4,26 @@ import 'package:skill_bit/core/constants/home_strings.dart';
 import 'package:skill_bit/core/theme/theme.dart';
 
 class SearchFieldWidget extends StatelessWidget {
-  const SearchFieldWidget({super.key, required this.width});
+  const SearchFieldWidget({
+    super.key,
+    required this.width,
+    required this.onChanged,
+    required this.function,
+    required this.controller,
+  });
+
   final double width;
+  final ValueChanged<String> onChanged;
+  final void Function()? function;
+  final TextEditingController controller;
 
   @override
   Widget build(final BuildContext context) {
     return SizedBox(
       width: width,
       child: SearchBar(
+        controller: controller,
+        onChanged: onChanged,
         elevation: WidgetStateProperty.all(0),
         side: WidgetStateProperty.all(
           BorderSide(
@@ -31,6 +43,12 @@ class SearchFieldWidget extends StatelessWidget {
           ),
         ),
         hintText: HomeStrings.search,
+        trailing: <Widget>[
+          IconButton(
+            onPressed: function,
+            icon: const FaIcon(FontAwesomeIcons.xmark),
+          ),
+        ],
       ),
     );
   }
