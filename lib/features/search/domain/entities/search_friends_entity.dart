ddcaae1c434f2ef0436cf037f9f18ffc9d679a9c
PHLY101 <basel6889@gmail.com>
2026-03-14 13:50:55 +0200

    created search course adn friend entities

---
+++ lib/features/search/domain/entities/search_friends_entity.dart
@@ -0,0 +1,13 @@
+class SearchFriendsEntity {
+  SearchFriendsEntity({
+    required this.name,
+    required this.profileUrl,
+    required this.scorePoints,
+    required this.progress,
+  });
+
+  final String name;
+  final String profileUrl;
+  final int scorePoints;
+  final double progress;
+}
