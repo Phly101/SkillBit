779b7e8ef7c3a6178ec38005dd3f8252d95c604b
PHLY101 <basel6889@gmail.com>
2026-03-20 01:47:16 +0200

    added widget to home path widget

---
+++ lib/core/widgets/line_progress_widget.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_colors.dart';
+
+class LineProgressWidget extends StatelessWidget {
+  const LineProgressWidget({
+    super.key,
+    required this.progress,
+    this.progressColor = AppColors.mainBlue,
+    this.minHeight =8,
+  });
+
+  final Color progressColor;
+  final double? progress;
+  final double? minHeight;
+
+  @override
+  Widget build(final BuildContext context) {
+    return LinearProgressIndicator(
+      value: progress,
+      backgroundColor: const Color(0xffCFDBE8),
+      valueColor: AlwaysStoppedAnimation<Color>(progressColor),
+      minHeight: 8,
+    );
+  }
+}
