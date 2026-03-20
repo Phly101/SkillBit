f195f9337239b7c2deb04d7e051a109944e11905
PHLY101 <basel6889@gmail.com>
2026-03-20 16:08:03 +0200

    updated status container

--- lib/features/home/presentation/widgets/common/status_container.dart
+++ lib/features/home/presentation/widgets/common/status_container.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import '../../../../../core/theme/theme.dart';
 
 class StatusContainer extends StatelessWidget {
-  const StatusContainer({super.key, required this.isLocked});
+  const StatusContainer({
+    super.key,
+    required this.isLocked,
+    required this.progress,
+  });
 
   final bool isLocked;
+  final double progress;
 
   @override
   Widget build(final BuildContext context) {
@@ -13,8 +18,13 @@ class StatusContainer extends StatelessWidget {
         borderRadius: .circular(30),
         color: context.colorScheme.secondary.withValues(alpha: 0.3),
       ),
+      //Todo: will be changed when the enrollment type be added to the course entity
       child: Text(
-        isLocked ? 'Locked' : 'Active',
+        isLocked
+            ? 'Locked'
+            : (progress == 1.0)
+            ? 'Finished'
+            : 'Active',
         style: context.textTheme.bodyMedium!.copyWith(
           color: context.colorScheme.primary,
         ),
