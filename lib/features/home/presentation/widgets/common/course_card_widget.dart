0cc3012c90c89500bbf6d0999eb4983f8b5319d9
PHLY101 <basel6889@gmail.com>
2026-03-21 19:45:17 +0200

    added tap to the course and hero tag with course id

--- lib/features/home/presentation/widgets/common/course_card_widget.dart
+++ lib/features/home/presentation/widgets/common/course_card_widget.dart
@@ -12,67 +12,77 @@ class CourseCardWidget extends StatelessWidget {
     required this.title,
     required this.progress,
     required this.isLocked,
+    required this.function,
+    required this.courseId,
   });
 
-  final String courseUrl;
+  final String? courseUrl;
+  final String courseId;
   final String title;
   final double progress;
   final bool isLocked;
+  final void Function()? function;
 
   @override
   Widget build(final BuildContext context) {
-    return ShadowContainer(
-      borderRadius: 25,
-      border: Border(
-        left: BorderSide(
-          color: isLocked ? Colors.transparent : context.colorScheme.primary,
-          width: 6,
+    return GestureDetector(
+      onTap: function,
+      child: ShadowContainer(
+        borderRadius: 25,
+        border: Border(
+          left: BorderSide(
+            color: isLocked ? Colors.transparent : context.colorScheme.primary,
+            width: 6,
+          ),
         ),
-      ),
-      child: Row(
-        children: <Widget>[
-          const SizedBox(width: 5),
-          Container(
-            width: MediaQuery.widthOf(context) * 0.35,
-            height: MediaQuery.heightOf(context) * 0.13,
-            decoration: BoxDecoration(
-              border: .all(width: 0.5, color: context.colorScheme.tertiary),
-              borderRadius: BorderRadius.circular(25),
-              color: context.colorScheme.secondary,
-              image: DecorationImage(
-                image: AssetImage(Assets.imageCourse(courseUrl)),
-                fit: .fill,
+        child: Row(
+          children: <Widget>[
+            const SizedBox(width: 5),
+            Hero(
+              tag: courseId,
+              child: Container(
+                width: MediaQuery.widthOf(context) * 0.35,
+                height: MediaQuery.heightOf(context) * 0.13,
+                decoration: BoxDecoration(
+                  border: .all(width: 0.5, color: context.colorScheme.tertiary),
+                  borderRadius: BorderRadius.circular(25),
+                  color: context.colorScheme.secondary,
+                  image: DecorationImage(
+                    image: AssetImage(Assets.imageCourse(courseUrl!)),
+                    fit: .fill,
+                  ),
+                ),
+              ).p10(),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: .start,
+                mainAxisSize: .min,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Text(
+                          title,
+                          style: context.textTheme.bodyLarge,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          softWrap: true,
+                        ),
+                      ),
+                      const SizedBox(width: 5),
+                      StatusContainer(isLocked: isLocked, progress: progress),
+                    ],
+                  ),
+                  const SizedBox(height: 10),
+                  LineProgressWidget(progress: progress),
+                ],
               ),
             ),
-          ).p10(),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: .start,
-              mainAxisSize: .min,
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: Text(
-                        title,
-                        style: context.textTheme.bodyLarge,
-                        maxLines: 2,
-                        overflow: TextOverflow.ellipsis,
-                        softWrap: true,
-                      ),
-                    ),
-                    const SizedBox(width: 5,),
-                    StatusContainer(isLocked: isLocked, progress: progress),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                LineProgressWidget(progress: progress),
-              ],
-            ),
-          ),
-        ],
-      ).p10(),
-    ).pNum(5);
+          ],
+        ).p10(),
+      ).pNum(5),
+    );
   }
 }
