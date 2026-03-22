2da4bafc385c178f2d6863f0aa599c17627ea644
PHLY101 <basel6889@gmail.com>
2026-03-22 19:59:54 +0200

    added a check if the app exists

--- lib/core/utils/global/launch_external_url.dart
+++ lib/core/utils/global/launch_external_url.dart
@@ -1,9 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class LaunchExternalUrl {
   static Future<void> launchExternalUrl(final Uri uri) async {
-    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
-      throw Exception('Could not launch $uri');
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      //Todo: handle the error later
+      debugPrint('Could not launch $uri');
     }
   }
 }
