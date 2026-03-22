ea08d9dae41909ed925c012341f9351d56fd6016
PHLY101 <basel6889@gmail.com>
2026-03-22 20:04:04 +0200

    added course constants

---
+++ lib/core/constants/course_constants.dart
@@ -0,0 +1,8 @@
+class CourseConstants {
+  CourseConstants._();
+
+  static const String title = 'Ready to delve into your first lesson?';
+  static const String description =
+      'Start your first lesson, read the description follow the web and youtube tutorials and test what you have learned with a quiz after each lesson. Good Luck!';
+  static const String buttonText = 'Start Learning!';
+}
