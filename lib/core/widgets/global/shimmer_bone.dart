18a6f7e1c7a6fe42849cee95adc560cb69b37e25
PHLY101 <basel6889@gmail.com>
2026-03-22 20:06:54 +0200

    added a shimmer global bone widget

---
+++ lib/core/widgets/global/shimmer_bone.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+
+
+class ShimmerBone extends StatelessWidget {
+  const ShimmerBone({
+    super.key,
+    required this.width,
+    required this.height,
+    this.borderRadius = 8,
+  });
+
+  final double width;
+  final double height;
+  final double borderRadius;
+
+  @override
+  Widget build(final BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[300]!,
+      highlightColor: Colors.grey[100]!,
+      child: Container(
+        width: width,
+        height: height,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+    );
+  }
+}
