392e7d8dcfd7afa1e2f2cd7de6d8c2b34d97478d
PHLY101 <basel6889@gmail.com>
2026-05-05 21:51:48 +0300

    added extra params required by verification and new password pages to properly send the api requests

--- lib/core/router/global_routes.dart
+++ lib/core/router/global_routes.dart
@@ -5,73 +5,83 @@ import 'package:skill_bit/core/router/routes.dart';
 
 import '../../features/features_index.dart'
     show
-    AssessmentTransitionPage,
-    ForgotPasswordPage,
-    LogInPage,
-    NewPasswordPage,
-    NotificationPage,
-    OnboardingPage,
-    SignUpPage,
-    SplashPage,
-    VerificationPage;
+        AssessmentTransitionPage,
+        ForgotPasswordPage,
+        LogInPage,
+        NewPasswordPage,
+        NotificationPage,
+        OnboardingPage,
+        SignUpPage,
+        SplashPage,
+        VerificationPage;
 import '../../features/onboarding/presentation/Bloc/onboarding_bloc.dart';
 
 class GlobalRoutes {
   // Routes accessible before/during authentication
-  static List<RouteBase> authRoutes(final OnboardingBloc onboardingBloc) =>
-      <RouteBase>[
-        GoRoute(
-          path: AppRoutes.splash,
-          builder: (final BuildContext context, final GoRouterState state) =>
+  static List<RouteBase> authRoutes(
+    final OnboardingBloc onboardingBloc,
+  ) => <RouteBase>[
+    GoRoute(
+      path: AppRoutes.splash,
+      builder: (final BuildContext context, final GoRouterState state) =>
           const SplashPage(),
-        ),
-        GoRoute(
-          path: AppRoutes.onBoarding,
-          builder: (final BuildContext context, final GoRouterState state) =>
+    ),
+    GoRoute(
+      path: AppRoutes.onBoarding,
+      builder: (final BuildContext context, final GoRouterState state) =>
           BlocProvider<OnboardingBloc>.value(
             value: onboardingBloc,
             child: const OnboardingPage(),
           ),
-        ),
-        GoRoute(
-          path: AppRoutes.login,
-          builder: (final BuildContext context, final GoRouterState state) =>
+    ),
+    GoRoute(
+      path: AppRoutes.login,
+      builder: (final BuildContext context, final GoRouterState state) =>
           const LogInPage(),
-        ),
-        GoRoute(
-          path: AppRoutes.signUp,
-          builder: (final BuildContext context, final GoRouterState state) =>
+    ),
+    GoRoute(
+      path: AppRoutes.signUp,
+      builder: (final BuildContext context, final GoRouterState state) =>
           const SignUpPage(),
-        ),
-        GoRoute(
-          path: AppRoutes.verification,
-          builder: (final BuildContext context, final GoRouterState state) =>
-          const VerificationPage(),
-        ),
-        GoRoute(
-          path: AppRoutes.forgotPassword,
-          builder: (final BuildContext context, final GoRouterState state) =>
+    ),
+    GoRoute(
+      path: AppRoutes.verification,
+      builder: (final BuildContext context, final GoRouterState state) {
+        final Map<String, dynamic> extra =
+            state.extra as Map<String, dynamic>? ?? <String, dynamic>{};
+        final String email = extra['email'] as String? ?? '';
+        final bool isResetPassword = extra['isReset'] as bool? ?? false;
+        return VerificationPage(email: email, isResetPassword: isResetPassword);
+      },
+    ),
+    GoRoute(
+      path: AppRoutes.forgotPassword,
+      builder: (final BuildContext context, final GoRouterState state) =>
           const ForgotPasswordPage(),
-        ),
-        GoRoute(
-          path: AppRoutes.newPassword,
-          builder: (final BuildContext context, final GoRouterState state) =>
-          const NewPasswordPage(),
-        ),
-      ];
+    ),
+    GoRoute(
+      path: AppRoutes.newPassword,
+      builder: (final BuildContext context, final GoRouterState state) {
+        final Map<String, dynamic> extra =
+            state.extra as Map<String, dynamic>? ?? <String, dynamic>{};
+        final String email = extra['email'] as String? ?? '';
+        final String code = extra['code'] as String? ?? '';
+        return NewPasswordPage(email: email, code: code);
+      },
+    ),
+  ];
 
   // Global utility routes
-  static List<RouteBase> utilityRoutes() =>
-      <RouteBase>[
-        GoRoute(
-          path: AppRoutes.notifications,
-          builder: (final BuildContext context, final GoRouterState state) =>
+  static List<RouteBase> utilityRoutes() => <RouteBase>[
+    GoRoute(
+      path: AppRoutes.notifications,
+      builder: (final BuildContext context, final GoRouterState state) =>
           const NotificationPage(),
-        ),
-        GoRoute(
-          path: AppRoutes.assessment,
-          builder: (final BuildContext context, final GoRouterState state) =>
+    ),
+    GoRoute(
+      path: AppRoutes.assessment,
+      builder: (final BuildContext context, final GoRouterState state) =>
           const AssessmentTransitionPage(),
-        ),
-      ];
+    ),
+  ];
 }
