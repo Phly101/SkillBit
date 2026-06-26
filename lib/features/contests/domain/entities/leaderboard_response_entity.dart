cc534361f327ec2c7452f86f7823a1d11e2c5e47
PHLY101 <basel6889@gmail.com>
2026-06-26 20:11:06 +0300

    created the entities that will hold the data of  the contest  feature

---
+++ lib/features/contests/domain/entities/leaderboard_response_entity.dart
@@ -0,0 +1,9 @@
+
+import 'leaderboard_entity.dart';
+
+class LeaderboardResponseEntity {
+
+  LeaderboardResponseEntity({required this.message, required this.leaderboard});
+  final String message;
+  final List<LeaderboardEntity> leaderboard;
+}
